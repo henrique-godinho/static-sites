aafc27ecf77ef5a91a0b00aede01545acff8e2cc
henrique-godinho <henrique.f.godinho@gmail.com>
2024-12-19 10:08:55 +0000

    first commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import *
+
+def main():
+    test = TextNode("node", "bold", "https://www.boot.dev")
+    
+main()
+
---
+++ src/test_textnode.py
@@ -0,0 +1,40 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertEqual(node, node2)
+
+    def test_url_eq(self):
+        node = TextNode("This is an url node", TextType.LINKS, "https://www.google.com")
+        node2 = TextNode("This is an url node", TextType.LINKS, "https://www.googe.com")
+
+    def test_not_eq(self):
+        node = TextNode("This is text node", TextType.NORMAL_TEXT)
+        node2 = TextNode("This is a text node", TextType.LINKS, "https://www.google.com")
+        self.assertNotEqual(node, node2)
+    
+    def test_text_not_eq(self):
+        node = TextNode("this is an image node", TextType.NORMAL_TEXT)
+        node2 = TextNode("This is a link node", TextType.NORMAL_TEXT)
+        self.assertNotEqual(node, node2)
+    
+    def test_type_not_eq(self):
+        node = TextNode("This is a text node", TextType.NORMAL_TEXT)
+        node2 = TextNode("This is a text node", TextType.IMAGES)
+        self.assertNotEqual(node, node2)
+    
+    def test_url_not_eq(self):
+        node = TextNode("This is an url node", TextType.LINKS, "https://google.com")
+        node2 = TextNode("This is an url node", TextType.LINKS, "http://google.co.uk")
+        self.assertNotEqual(node, node2)
+    
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
